b6e65ab35993da337127617d194ffa41df773ec9
Vladyslav Tkachuk <vladyslav_tkachuk1@epam.com>
2021-01-04 11:41:42 +0200

    project cheated

---
+++ Pokemons/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Pokemons
+//
+//  Created by Vladyslav Tkachuk1 on 1/4/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Pokemons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemons
+//
+//  Created by Vladyslav Tkachuk1 on 1/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
