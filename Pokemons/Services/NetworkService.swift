6572b8d510f6935c0486a448a622057afca086ac
Vladyslav Tkachuk <vladyslav_tkachuk1@epam.com>
2021-01-04 12:50:39 +0200

    created base network servise

---
+++ Pokemons/Services/NetworkService.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkService.swift
+//  Pokemons
+//
+//  Created by Vladyslav Tkachuk1 on 1/4/21.
+//
+
+import Foundation
+
+enum RequestError: Error {
+    case badRequest
+    case noData
+    case unknownError
+}
+
+final class NetworkService {
+    static func request<T: Decodable>(query: URL, completion: @escaping (Result<T, Error>) -> Void) {
+        URLSession.shared.dataTask(with: query) { (data, response, error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(RequestError.badRequest))
+                return
+            }
+            guard let data = data else {
+                return completion(.failure(RequestError.noData))
+            }
+            if (200...299).contains(response.statusCode) {
+                do {
+                    let responseObject = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(responseObject))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            }
+            completion(.failure(RequestError.unknownError))
+        }.resume()
+    }
+}
